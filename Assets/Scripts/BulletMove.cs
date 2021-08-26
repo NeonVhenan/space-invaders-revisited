7e3dab53d09f0faba3f22f2264ce51c20ac18685
Pauline C <47113664+NeonVhenan@users.noreply.github.com>
2021-08-26 16:25:30 +0200

    Add files via upload

---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+
+    private Rigidbody2D myRigid;
+    GameStateManager gsm;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myRigid = this.GetComponent<Rigidbody2D>();
+
+        gsm = GameObject.Find("GameState").GetComponent<GameStateManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        myRigid.AddForce(this.transform.up * 25);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject gob = collision.gameObject;
+
+        if (gob.tag == "Alien")
+        {
+            gob.GetComponent<MoveAlien>().StarScale();
+        }
+
+
+    }
+
+}
